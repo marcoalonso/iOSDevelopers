aa9a2d5da629cf6831112dd974073df9962cdbee
marcoalonso <marcoalonsorota@hotmail.com>
2021-11-25 15:03:18 -0600

    GettingStarted

---
+++ Learn By Doing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learn By Doing
+//
+//  Created by marco rodriguez on 25/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learn By Doing/Learn_By_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_By_DoingApp.swift
+//  Learn By Doing
+//
+//  Created by marco rodriguez on 25/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_By_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
